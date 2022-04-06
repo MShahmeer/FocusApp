69bb593d474bd05e970cd00cb6ea12c3cfa4287a
Muhammad Shahmeer <91695394+MShahmeer@users.noreply.github.com>
2022-04-06 23:45:41 +0500

    Focus Time App

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import { Focus } from "./src/features/focus/Focus";
+import { Timer } from "./src/features/timer/Timer";
+import { colors } from "./src/utils/colors";
+import { spacing } from "./src/utils/sizes";
 export default function App() {
+  const [focusSubject, setFocusedSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  useEffect(() => {
+    if(focusSubject){
+      setFocusHistory([...focusHistory,focusSubject])
+    }
+  },[focusSubject])
+
+  console.log(focusHistory)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {focusSubject ? (
+        <Timer
+          focusSubject={focusSubject}
+          onTimerEnd={() => {
+            setFocusedSubject(null);
+          }}
+
+          clearSubject = {() => setFocusedSubject(null)}
+        />
+      ) : (
+        <Focus addSubject={setFocusedSubject} />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Platform.OS === "ios" ? spacing.md : spacing.lg,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.darkBlue,
   },
 });
